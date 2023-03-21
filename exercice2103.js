cd3ba47203c87f458d2cc7874a6fcd5a1d139d94
Abdel-jalil212 <jalil.elouarroudi@gmail.com>
2023-03-21 16:58:36 +0100

    Create exercice2103.js

---
+++ exercice2103.js
@@ -0,0 +1,13 @@
+var age= 25 //  Exercice 1
+var nom= "Jean" // Exercice 2
+var somme= 24 + 95 // Exercice 3
+var chaine= "Ceci est un exercice"// Exercice 4
+var result= chaine.toUpperCase()
+let tableau= [1,2,3,4,5]
+console.log(tableau.length)// Exercice 5
+let objet= [nom: "Jean", age: 25]
+console.log(objet.nom)// Exercice 6
+var nombre= 95.2727
+nombre.toFixed(2)// Exercice 7
+var date= Date.now()
+console.log (getMonth)// Exercice 8
